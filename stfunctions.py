f9d0cfccd0f7d1440590cbb7eb4395f9910dab6e
tomikshk <tomiriskushekenova@gmail.com>
2026-04-20 17:42:35 +0800

    Create stfunctions.py

---
+++ stfunctions.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Welcome to Streamlit!")
+
+st.write("Hello, Streamlit!")
+st.write(12345)
+st.write({"Name": "Alice", "Age": 30})
+
+st.write(['list1', 'list2'])
+
+st.write("**Bold Text** and *Italic Text*")
+
+
